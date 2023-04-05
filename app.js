5622476d87e2f2535970acb6a2385cdfc6d17b53
peytontan <peytontanhs@gmail.com>
2023-04-06 00:07:47 +0800

    uncommented bg colour for now since i know it works

---
+++ app.js
@@ -0,0 +1,32 @@
+// $(()=>{
+//     class $grids {
+//         constructor ($row,$col){
+
+//         }
+//     }
+//     // console.log("test")//this works
+
+
+
+
+// })
+
+
+// class Grids{
+//     constructor(row, col){
+//         this.row = row
+//         this.col = col
+//     }
+//     getGrid (){
+//         for (let i=0 ; i <this.row;i++){
+//             console.log(i)
+//             }
+//         for (let x = 0; x <this.col; x ++){
+//             console.log(x)
+//         }
+//     }
+// }
+
+// const board = new Grids(5,5)
+// console.log(board.getGrid())
+
