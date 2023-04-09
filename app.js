29d247f3807a7894f841cad7df55d5e442d4bff6
peytontan <peytontanhs@gmail.com>
2023-04-09 19:04:50 +0800

    players are able to take turns, but once a user has clicked, user will need to click another time in order for the pc to try, will need to fix it

--- app.js
+++ app.js
@@ -32,6 +32,7 @@ $(()=>{
         })
         $pcBattleshipBoard.hide() //hide this later on, if we wanna cheat we can check in elements hehe
     })
+    
 
     //make a function to toggle between user and pc turn
     const toggleTurns = () => {
@@ -45,29 +46,134 @@ $(()=>{
         const $battleShipBoardToClick = $('.boardToClick')
         $battleShipBoardToClick.append($gridToClick)
     })
-    
+    //code above works do not touch
+    //------------------------------------------------------------------------------------------------
+
     shipPC.forEach(element => { //this is for when the user is clicking on the enemy's ship and it matches the same grid that the enemy has placed their ship at, it will turn the grid red colour
         element.location.forEach(cell => {
             $(`#${cell}.clickGrid`).on('click',()=>{
-                $(`#${cell}.clickGrid`).css("background-color","red")
-                $(`#${cell}.clickGrid`).toggleClass("hit")
-                alert(`${cell} was hit`)
-                turnCounter+=1
-                console.log(turnCounter)
-                $(`#${cell}.clickGrid`).off('click') //this will stop the grid that was already clicked on to stopped being counted as a turn, and it will not allow to change the turn unless a valid grid has been clicked
+                if (userTurn){
+                    $(`#${cell}.clickGrid`).css("background-color","red")
+                    $(`#${cell}.clickGrid`).toggleClass("hit")
+                    alert(`grid ${cell} was hit`)
+                    turnCounter+=1
+                    console.log(turnCounter)
+                    $(`#${cell}.clickGrid`).off('click') //this will stop the grid that was already clicked on to stopped being counted as a turn, and it will not allow to change the turn unless a valid grid has been clicked
+                    toggleTurns()
+                } else {
+                    let availableIndexesPC = userBoard.gridIndex
+                    let index = Math.floor(Math.random() * board.gridIndex.length)-1
+                    let gridIndex = availableIndexesPC[index]   
+                        if (userTakenGrids.includes(gridIndex)){
+                            console.log(gridIndex)
+                            $(`#${gridIndex}.userGrid`).css("background-color","orange").toggleClass("hit")
+                            availableIndexesPC = availableIndexesPC.filter((element) => element!==gridIndex) //removing the already selected index so that it will not be used to select the grid again
+                            // console.log("availble index", availableIndexesPC)
+                            toggleTurns()
+                        } else {
+                            console.log(gridIndex)
+                            $(`#${gridIndex}.userGrid`).css("background-color","green").toggleClass("missed")
+                            availableIndexesPC = availableIndexesPC.filter((element) => element!==gridIndex) //removing the already selected index so that it will not be used to select the grid again
+                            // console.log("availble index",availableIndexesPC)
+                            toggleTurns()
+                    }
+                }
             })
         }) 
     })
-    
+        
     pcNotTakenGrids.forEach(cell => { //to  see which of the enemy grids are not occupied, and when its clicked on, we need to change it to green because it means that it wasn't hit on 
         $(`#${cell}.clickGrid`).on('click',()=>{
-            $(`#${cell}.clickGrid`).css("background-color","green")
-            $(`#${cell}.clickGrid`).toggleClass("missed")
-            turnCounter+=1
-            console.log(turnCounter)
-            $(`#${cell}.clickGrid`).off('click') //this will stop the grid that was already clicked on to stopped being counted as a turn, and it will not allow to change the turn unless a valid grid has been clicked
+            if (userTurn){
+                $(`#${cell}.clickGrid`).css("background-color","green")
+                $(`#${cell}.clickGrid`).toggleClass("missed")
+                turnCounter+=1
+                console.log(turnCounter)
+                $(`#${cell}.clickGrid`).off('click') //this will stop the grid that was already clicked on to stopped being counted as a turn, and it will not allow to change the turn unless a valid grid has been clicked
+                // console.log(userTurn)
+                toggleTurns()
+                // console.log(userTurn)
+            } else {
+                let availableIndexesPC = userBoard.gridIndex
+                let index = Math.floor(Math.random() * board.gridIndex.length)-1
+                let gridIndex = availableIndexesPC[index]   
+                    if (userTakenGrids.includes(gridIndex)){
+                        console.log(gridIndex)
+                        $(`#${gridIndex}.userGrid`).css("background-color","orange").toggleClass("hit")
+                        availableIndexesPC = availableIndexesPC.filter((element) => element!==gridIndex) //removing the already selected index so that it will not be used to select the grid again
+                        // console.log("availble index", availableIndexesPC)
+                        toggleTurns()
+                    } else {
+                        console.log(gridIndex) //will need to check why there is an undefined
+                        $(`#${gridIndex}.userGrid`).css("background-color","green").toggleClass("missed")
+                        availableIndexesPC = availableIndexesPC.filter((element) => element!==gridIndex) //removing the already selected index so that it will not be used to select the grid again
+                        // console.log("availble index",availableIndexesPC)
+                        toggleTurns()
+                }
+            }
         })
     })
+    
+    
+    
+    // if (userTurn===false) { //pc's turn
+        //randomly select
+    //     let index = Math.floor(Math.random() * board.gridIndex.length)-1
+    //     let gridIndex = userBoard.gridIndex[index]
+    //     if (userTakenGrids.includes(gridIndex)){
+    //         console.log(gridIndex)
+    //         $(`#${gridIndex}.userGrid`).css("background-color","orange").toggleClass("hit")
+    //         toggleTurns()
+    //     } else {
+    //         console.log(gridIndex)
+    //         $(`#${gridIndex}.userGrid`).css("background-color","green").toggleClass("missed")
+    //         toggleTurns()
+    // }
+
+
+    //code below works do not touch
+    //-------------------------------------------------------------------------------------------------------------------
+    //this is the old code, if we are to remove the if portion then we can just check this
+    // shipPC.forEach(element => { //this is for when the user is clicking on the enemy's ship and it matches the same grid that the enemy has placed their ship at, it will turn the grid red colour
+    //     element.location.forEach(cell => {
+    //         $(`#${cell}.clickGrid`).on('click',()=>{
+    //             $(`#${cell}.clickGrid`).css("background-color","red")
+    //             $(`#${cell}.clickGrid`).toggleClass("hit")
+    //             alert(`grid ${cell} was hit`)
+    //             turnCounter+=1
+    //             console.log(turnCounter)
+    //             $(`#${cell}.clickGrid`).off('click') //this will stop the grid that was already clicked on to stopped being counted as a turn, and it will not allow to change the turn unless a valid grid has been clicked
+    //         })
+    //     }) 
+    // })
+    
+    // pcNotTakenGrids.forEach(cell => { //to  see which of the enemy grids are not occupied, and when its clicked on, we need to change it to green because it means that it wasn't hit on 
+    //     $(`#${cell}.clickGrid`).on('click',()=>{
+    //         $(`#${cell}.clickGrid`).css("background-color","green")
+    //         $(`#${cell}.clickGrid`).toggleClass("missed")
+    //         turnCounter+=1
+    //         console.log(turnCounter)
+    //         $(`#${cell}.clickGrid`).off('click') //this will stop the grid that was already clicked on to stopped being counted as a turn, and it will not allow to change the turn unless a valid grid has been clicked
+    //     })
+    // })
+
+    //pc randomly selecting
+    //create a new array for userboard's gridindex
+    // let availableIndexesPC = userBoard.gridIndex
+    // let index = Math.floor(Math.random() * board.gridIndex.length)-1
+    // let gridIndex = availableIndexesPC[index]   
+    //     if (userTakenGrids.includes(gridIndex)){
+    //         console.log(gridIndex)
+    //         $(`#${gridIndex}.userGrid`).css("background-color","orange").toggleClass("hit")
+    //         availableIndexesPC = availableIndexesPC.filter((element) => element!==gridIndex) //removing the already selected index so that it will not be used to select the grid again
+    //         // console.log("availble index", availableIndexesPC)
+    //     } else {
+    //         console.log(gridIndex)
+    //         $(`#${gridIndex}.userGrid`).css("background-color","green").toggleClass("missed")
+    //         availableIndexesPC = availableIndexesPC.filter((element) => element!==gridIndex) //removing the already selected index so that it will not be used to select the grid again
+    //         // console.log("availble index",availableIndexesPC)
+    // }
+
     
     
     // $('.turnCount').text(`Turn Count = ${turnCounter}`)
