275405b24b80ea36715aedeab395c9ee4a045811
peytontan <peytontanhs@gmail.com>
2023-04-09 19:49:02 +0800

    used the 2 new variables and counter is now working

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 import { createBoard, arrayAlphabets, arrayNumbers, board,userBoard, gameBoard } from "./grid.js"
-import {pcTakenGrids, userTakenGrids, Ship, carrierUser, battleshipUser, cruiserUser, submarineUser,destroyerUser,shipsUser,carrierPC,battleshipPC,cruiserPC,submarinePC,destroyerPC,shipPC,userNotTakenGrids,pcNotTakenGrids} from "./ship.js"
+import {pcTakenGrids, userTakenGrids, Ship, carrierUser, battleshipUser, cruiserUser, submarineUser,destroyerUser,shipsUser,carrierPC,battleshipPC,cruiserPC,submarinePC,destroyerPC,shipPC,userNotTakenGrids,pcNotTakenGrids,userGrids,pcGrids} from "./ship.js"
 
 
 $(()=>{
     let userTurn = true //this allows us to keep track of whose turn it is
     let turnCounter = 0
+    $('.turnCount').text(`Turn Count = ${turnCounter}`)
 
-    // $('.turnCount').text(`Turn Count = ${turnCounter}`)
     
     //user's board
     userBoard.gridIndex.forEach(grid=>{ //this is to show the randomly generated grids for user
@@ -15,7 +15,7 @@ $(()=>{
         $battleShipBoard.append($gridToPlace)
         shipsUser.forEach(element => {
             element.location.forEach(cell => {
-                $(`#${cell}.userGrid`).css("background-color","red") //need to think of ways to stop it from overlapping
+                $(`#${cell}.userGrid`).css("background-color","grey") //need to think of ways to stop it from overlapping
             })
         })
     })
@@ -49,38 +49,41 @@ $(()=>{
     //code above works do not touch
     //------------------------------------------------------------------------------------------------
 
-    shipPC.forEach(element => { //this is for when the user is clicking on the enemy's ship and it matches the same grid that the enemy has placed their ship at, it will turn the grid red colour
-        element.location.forEach(cell => {
-            $(`#${cell}.clickGrid`).on('click',()=>{
-                if (userTurn){
-                    $(`#${cell}.clickGrid`).css("background-color","red")
-                    $(`#${cell}.clickGrid`).toggleClass("hit")
-                    alert(`grid ${cell} was hit`)
-                    turnCounter+=1
-                    console.log(turnCounter)
-                    $(`#${cell}.clickGrid`).off('click') //this will stop the grid that was already clicked on to stopped being counted as a turn, and it will not allow to change the turn unless a valid grid has been clicked
-                    toggleTurns()
-                } else {
-                    let availableIndexesPC = userBoard.gridIndex
-                    let index = Math.floor(Math.random() * board.gridIndex.length)-1
-                    let gridIndex = availableIndexesPC[index]   
-                        if (userTakenGrids.includes(gridIndex)){
-                            console.log(gridIndex)
-                            $(`#${gridIndex}.userGrid`).css("background-color","orange").toggleClass("hit")
-                            availableIndexesPC = availableIndexesPC.filter((element) => element!==gridIndex) //removing the already selected index so that it will not be used to select the grid again
-                            // console.log("availble index", availableIndexesPC)
-                            toggleTurns()
-                        } else {
-                            console.log(gridIndex)
-                            $(`#${gridIndex}.userGrid`).css("background-color","green").toggleClass("missed")
-                            availableIndexesPC = availableIndexesPC.filter((element) => element!==gridIndex) //removing the already selected index so that it will not be used to select the grid again
-                            // console.log("availble index",availableIndexesPC)
-                            toggleTurns()
-                    }
+ //this is for when the user is clicking on the enemy's ship and it matches the same grid that the enemy has placed their ship at, it will turn the grid red colour
+ pcGrids.forEach(cell => {
+        $(`#${cell}.clickGrid`).on('click',()=>{
+            if (userTurn){
+                $(`#${cell}.clickGrid`).css("background-color","red")
+                $(`#${cell}.clickGrid`).toggleClass("hit")
+                alert(`grid ${cell} was hit`)
+                turnCounter+=1
+                $('.turnCount').text(`Turn Count = ${turnCounter}`)
+                $(`#${cell}.clickGrid`).off('click') //this will stop the grid that was already clicked on to stopped being counted as a turn, and it will not allow to change the turn unless a valid grid has been clicked
+                toggleTurns()
+            } else {
+                let availableIndexesPC = userBoard.gridIndex
+                let index = Math.floor(Math.random() * board.gridIndex.length)-1
+                let gridIndex = availableIndexesPC[index]   
+                    if (userGrids.includes(gridIndex)){
+                        console.log(gridIndex)
+                        $(`#${gridIndex}.userGrid`).css("background-color","red").toggleClass("hit")
+                        availableIndexesPC = availableIndexesPC.filter((element) => element!==gridIndex) //removing the already selected index so that it will not be used to select the grid again
+                        // console.log("availble index", availableIndexesPC)
+                        turnCounter+=1
+                        $('.turnCount').text(`Turn Count = ${turnCounter}`)
+                        toggleTurns()
+                    } else {
+                        console.log(gridIndex)
+                        $(`#${gridIndex}.userGrid`).css("background-color","green").toggleClass("missed")
+                        availableIndexesPC = availableIndexesPC.filter((element) => element!==gridIndex) //removing the already selected index so that it will not be used to select the grid again
+                        // console.log("availble index",availableIndexesPC)
+                        turnCounter+=1
+                        $('.turnCount').text(`Turn Count = ${turnCounter}`)
+                        toggleTurns()
                 }
-            })
-        }) 
-    })
+            }
+        })
+    }) 
         
     pcNotTakenGrids.forEach(cell => { //to  see which of the enemy grids are not occupied, and when its clicked on, we need to change it to green because it means that it wasn't hit on 
         $(`#${cell}.clickGrid`).on('click',()=>{
@@ -88,47 +91,35 @@ $(()=>{
                 $(`#${cell}.clickGrid`).css("background-color","green")
                 $(`#${cell}.clickGrid`).toggleClass("missed")
                 turnCounter+=1
-                console.log(turnCounter)
                 $(`#${cell}.clickGrid`).off('click') //this will stop the grid that was already clicked on to stopped being counted as a turn, and it will not allow to change the turn unless a valid grid has been clicked
                 // console.log(userTurn)
+                $('.turnCount').text(`Turn Count = ${turnCounter}`)
                 toggleTurns()
                 // console.log(userTurn)
             } else {
                 let availableIndexesPC = userBoard.gridIndex
                 let index = Math.floor(Math.random() * board.gridIndex.length)-1
                 let gridIndex = availableIndexesPC[index]   
-                    if (userTakenGrids.includes(gridIndex)){
+                    if (userGrids.includes(gridIndex)){
                         console.log(gridIndex)
-                        $(`#${gridIndex}.userGrid`).css("background-color","orange").toggleClass("hit")
+                        $(`#${gridIndex}.userGrid`).css("background-color","red").toggleClass("hit")
                         availableIndexesPC = availableIndexesPC.filter((element) => element!==gridIndex) //removing the already selected index so that it will not be used to select the grid again
                         // console.log("availble index", availableIndexesPC)
+                        turnCounter+=1
+                        $('.turnCount').text(`Turn Count = ${turnCounter}`)
                         toggleTurns()
                     } else {
                         console.log(gridIndex) //will need to check why there is an undefined
                         $(`#${gridIndex}.userGrid`).css("background-color","green").toggleClass("missed")
                         availableIndexesPC = availableIndexesPC.filter((element) => element!==gridIndex) //removing the already selected index so that it will not be used to select the grid again
                         // console.log("availble index",availableIndexesPC)
+                        turnCounter+=1
+                        $('.turnCount').text(`Turn Count = ${turnCounter}`)
                         toggleTurns()
                 }
             }
         })
     })
-    
-    
-    
-    // if (userTurn===false) { //pc's turn
-        //randomly select
-    //     let index = Math.floor(Math.random() * board.gridIndex.length)-1
-    //     let gridIndex = userBoard.gridIndex[index]
-    //     if (userTakenGrids.includes(gridIndex)){
-    //         console.log(gridIndex)
-    //         $(`#${gridIndex}.userGrid`).css("background-color","orange").toggleClass("hit")
-    //         toggleTurns()
-    //     } else {
-    //         console.log(gridIndex)
-    //         $(`#${gridIndex}.userGrid`).css("background-color","green").toggleClass("missed")
-    //         toggleTurns()
-    // }
 
 
     //code below works do not touch
@@ -162,7 +153,7 @@ $(()=>{
     // let availableIndexesPC = userBoard.gridIndex
     // let index = Math.floor(Math.random() * board.gridIndex.length)-1
     // let gridIndex = availableIndexesPC[index]   
-    //     if (userTakenGrids.includes(gridIndex)){
+    //     if (userGrids.includes(gridIndex)){
     //         console.log(gridIndex)
     //         $(`#${gridIndex}.userGrid`).css("background-color","orange").toggleClass("hit")
     //         availableIndexesPC = availableIndexesPC.filter((element) => element!==gridIndex) //removing the already selected index so that it will not be used to select the grid again
@@ -173,8 +164,6 @@ $(()=>{
     //         availableIndexesPC = availableIndexesPC.filter((element) => element!==gridIndex) //removing the already selected index so that it will not be used to select the grid again
     //         // console.log("availble index",availableIndexesPC)
     // }
-
-    
     
     // $('.turnCount').text(`Turn Count = ${turnCounter}`)
 })
